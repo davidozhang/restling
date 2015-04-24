6e812f7922a402d26466e5b60c5738f8241a7373
David Zhang <davzee@hotmail.com>
2015-04-23 22:01:36 -0400

    Added /lyrics public endpoint that jsonifies lyrics

--- restling/__init__.py
+++ restling/__init__.py
@@ -1,10 +1,37 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:restling@localhost/restling'
+
+from restling.utils import serializer
+
+
 @app.route('/')
 def index():
-	return 'Restling'
+	return 'restling api index page.'
+
+@app.route('/ping')
+def ping():
+	return 'restling is up and running!'
+
+@app.route('/lyrics', methods=['GET'])
+def lyrics():
+	# Publicly available endpoint
+	if 'artist' not in request.args or 'track' not in request.args:
+		result = {
+			'status': 'error',
+			'message': 'invalid request.',
+			'data': None,
+		}
+		return serializer.dumps(result)
+
+	from restling.services.lyrics import Lyrics
+	l = Lyrics(artist=request.args['artist'], track=request.args['track'])
+	return serializer.dumps(l.result)
 
 if __name__ == '__main__':
-	app.run()
-
+	app.run(debug=True)
---
+++ restling/utils/serializer.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def dumps(data):
+    return json.dumps(data)
+
+
+def loads(json_data):
+    return json.loads(json_data)
